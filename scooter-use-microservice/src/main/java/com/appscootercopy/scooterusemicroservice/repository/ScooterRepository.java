0f3baa8ae6ae265b181ae03d8ec87d73f06f8954
Brian Freijomil <brianfreijomil@hotmail.com>
2023-10-28 17:26:19 -0300

    "report scooters by kms"

--- scooter-use-microservice/src/main/java/com/appscootercopy/scooterusemicroservice/repository/ScooterRepository.java
+++ scooter-use-microservice/src/main/java/com/appscootercopy/scooterusemicroservice/repository/ScooterRepository.java
@@ -2,6 +2,7 @@ package com.appscootercopy.scooterusemicroservice.repository;
 
 import com.appscootercopy.scooterusemicroservice.domain.Scooter;
 import com.appscootercopy.scooterusemicroservice.domain.Ubication;
+import com.appscootercopy.scooterusemicroservice.service.dto.scooter.response.ReportUseScootersByKmsDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +12,10 @@ import java.util.List;
 public interface ScooterRepository extends JpaRepository<Scooter, Long> {
 
     Scooter findByLicensePLate(String licensePlate);
+
     Boolean existsByLicensePLate(String licensePlate);
 
     @Query("SELECT s FROM Scooter s JOIN fetch s.ubication")
     List<Scooter> findAllfetchingUbication();
-
 
 }
