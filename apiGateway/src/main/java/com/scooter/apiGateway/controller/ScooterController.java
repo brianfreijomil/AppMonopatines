5afdfbd5b869c0e3b4f249b5e06de3933213c03a
benjamin <benjamin.official.2019@gmail.com>
2023-10-28 20:06:12 -0300

    cambios

--- apiGateway/src/main/java/com/scooter/apiGateway/controller/ScooterController.java
+++ apiGateway/src/main/java/com/scooter/apiGateway/controller/ScooterController.java
@@ -2,9 +2,12 @@ package com.scooter.apiGateway.controller;
 
 import com.scooter.apiGateway.service.Constants;
 import com.scooter.apiGateway.service.ScooterService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/scooter")
@@ -22,5 +25,83 @@ public class ScooterController {
         return scooterService.setMaintenance(licensePlate, setMaintenance);
     }
 
-    @PostMapping("/ubication")
+    @GetMapping("/{licensePlate}")
+    public ScooterResponseDTO getScooterByLicensePlate(@PathVariable String licensePlate){
+        return scooterService.findScooterByLicensePlate(licensePlate);
+    }
+
+    @GetMapping("/")
+    public List<ScooterResponseDTO> getAllScooter(){
+        return this.scooterService.findAllScooter();
+    }
+
+    @PostMapping("")
+    public ResponseEntity saveScooter(@RequestBody @Valid ScooterRequestDTO request){
+        return scooterService.saveScooter(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteScooter(@PathVariable Long id){
+        this.scooterService.deleteScooter(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateScooter(@RequestBody @Valid ScooterRequestDTO request, @PathVariable Long id){
+        return this.scooterService.updateScooter(request, id);
+    }
+
+    @GetMapping("/stops/{ubicationId}")
+    public ScooterStopResponseDTO getScooterStopByUbication(@PathVariable Long ubicationId){
+        return scooterService.findScooterStopByUbication(ubicationId);
+    }
+
+    @GetMapping("/stops/")
+    public List<ScooterStopResponseDTO> getAllScooterStop(){
+        return this.scooterService.findAllScooterStop();
+    }
+
+    @PostMapping("/stops")
+    public ResponseEntity saveScooterStop(@RequestBody @Valid ScooterStopRequestDTO request) {
+        return this.scooterService.saveScooterStop(request);
+    }
+
+    @DeleteMapping("/stops/{id}")
+    public void deleteScooterStop(@PathVariable Long id){
+        this.scooterService.deleteScooterStop(id);
+    }
+
+    @PutMapping("/stops/{id}")
+    public ResponseEntity updateScooterStop(@RequestBody @Valid ScooterStopRequestDTO request, @PathVariable Long id){
+        return this.scooterService.updateScooterStop(request, id);
+    }
+
+    @GetMapping("/{id}/trip/{idTrip}")
+    public ScooterTripResponseDTO getScooterTripById(@PathVariable Long id, @PathVariable Long idTrip) {
+        return this.scooterService.findScooterTripById(id,idTrip);
+    }
+
+    @GetMapping("/trips")
+    public List<ScooterTripResponseDTO> getAllScooterTrip() {
+        return this.scooterService.findAllScooterTrip();
+    }
+
+    @GetMapping("/{id}/trips")
+    public List<ScooterTripResponseDTO> getAllScooterTripByScooterId(@PathVariable Long id) {
+        return this.scooterService.findAllScooterTripByScooterId(id);
+    }
+
+    @PostMapping("/trip")
+    public ResponseEntity saveScooterTrip(@RequestBody @Valid ScooterTripRequestDTO request) {
+        return this.scooterService.saveScooterTrip(request);
+    }
+
+    @GetMapping("/ubications/{id}")
+    public UbicationResponseDTO getUbicationById(@PathVariable Long id){
+        return scooterService.findUbicationById(id);
+    }
+
+    @GetMapping("/ubications/")
+    public List<UbicationResponseDTO> getAllUbication(){
+        return this.scooterService.findAllUbication();
+    }
 }
