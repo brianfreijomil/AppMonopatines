0d440c7ba90aed81de33483b5600e2625137c801
Lucas Amendola <lukitasromanpiles7@gmail.com>
2023-10-31 18:48:42 -0300

    final issues

---
+++ UserMicroservice/src/main/java/com/ScootersApp/Service/exception/ConflictWithStatusException.java
@@ -0,0 +1,9 @@
+package com.ScootersApp.Service.exception;
+
+public class ConflictWithStatusException extends RuntimeException{
+    private String message;
+
+    public ConflictWithStatusException(String entity, String attribute, int status) {
+        this.message = String.format("The status of this %s entity is already %s %s.", entity, attribute, status);
+    }
+}
