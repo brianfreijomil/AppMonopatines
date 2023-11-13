4219014db1ea6f17e331792857583d1e79f3c294
Lucas Amendola <lukitasromanpiles7@gmail.com>
2023-11-13 17:04:07 -0300

    cambio insercion de roles en user

--- UserMicroservice/src/main/java/com/ScootersApp/config/JWTFilter.java
+++ UserMicroservice/src/main/java/com/ScootersApp/config/JWTFilter.java
@@ -5,7 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-//import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HTTP;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
