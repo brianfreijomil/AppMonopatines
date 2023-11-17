44f2f8a6036409a1daa6dacc931f6b7bf277b230
Brian Freijomil <brianfreijomil@hotmail.com>
2023-11-17 19:05:08 -0300

    trip test

--- trip-microservice/src/test/java/com/appscooter/tripmicroservice/TripMicroserviceApplicationTests.java
+++ trip-microservice/src/test/java/com/appscooter/tripmicroservice/TripMicroserviceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.appscooter.tripmicroservice;
 
+import com.appscooter.tripmicroservice.controllers.TripController;
+import com.appscooter.tripmicroservice.services.dtos.trip.responses.TripResponseDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class TripMicroserviceApplicationTests {
 
+	private TripController controller;
+
+	@Autowired
+	public TripMicroserviceApplicationTests(TripController tp) {
+		controller=tp;
+	}
+
 	@Test
-	void contextLoads() {
+	void testGetTrips() {
+		List<TripResponseDTO> listResult = controller.findAllTrip();
+		Assert.isTrue(listResult.isEmpty(), "vacia minga");
 	}
 
 }
