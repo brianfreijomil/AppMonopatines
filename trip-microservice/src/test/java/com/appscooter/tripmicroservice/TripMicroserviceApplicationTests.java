8a2030c62ba641a437cb4f9b07cce28c89df7818
Brian Freijomil <brianfreijomil@hotmail.com>
2023-11-17 22:20:28 -0300

    test en scooter

--- trip-microservice/src/test/java/com/appscooter/tripmicroservice/TripMicroserviceApplicationTests.java
+++ trip-microservice/src/test/java/com/appscooter/tripmicroservice/TripMicroserviceApplicationTests.java
@@ -1,12 +1,20 @@
 package com.appscooter.tripmicroservice;
 
 import com.appscooter.tripmicroservice.controllers.TripController;
+import com.appscooter.tripmicroservice.services.dtos.tariff.request.TotalProfitsRequestDTO;
+import com.appscooter.tripmicroservice.services.dtos.tariff.response.ReportProfitsDTO;
+import com.appscooter.tripmicroservice.services.dtos.trip.requests.TripRequestDTO;
 import com.appscooter.tripmicroservice.services.dtos.trip.responses.TripResponseDTO;
+import com.appscooter.tripmicroservice.services.exception.ConflictExistException;
+import com.appscooter.tripmicroservice.services.exception.NotFoundException;
+import com.appscooter.tripmicroservice.services.exception.WebClientNotFound;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.testng.Assert;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
@@ -20,9 +28,38 @@ class TripMicroserviceApplicationTests {
 	}
 
 	@Test
-	void testGetTrips() {
-		List<TripResponseDTO> listResult = controller.findAllTrip();
-		Assert.isTrue(listResult.isEmpty(), "vacia minga");
+	void testGetTripById() {
+		try {
+			//obtengo trip por id
+			TripResponseDTO t = controller.findTripById(24L);
+		}
+		catch (NotFoundException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	void testSaveTrip() {
+		TotalProfitsRequestDTO request = new TotalProfitsRequestDTO();
+		request.setFirstMonth(1L); //mes enero
+		request.setLastMonth(6L); //mes junio
+		request.setYear(2023L);  //anio
+		List<ReportProfitsDTO> report = controller.findProfitsByMonthsInYear(request);
+		for (ReportProfitsDTO result : report) {
+			System.out.println(result);
+		}
+	}
+
+	@Test
+	void testDeleteTripById() {
+		Long id = 12L;
+		try {
+			controller.deleteTrip(12L);
+			controller.deleteTrip(20L);
+		}
+		catch (NotFoundException e) {
+			System.out.println(e);
+		}
 	}
 
 }
